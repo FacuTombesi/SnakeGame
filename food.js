b2cc695582292400375b4be6c9b44cfbeafe75a6
FacuTombesi <facundomtombesi@gmail.com>
2023-06-08 18:12:50 -0300

    food.js/grid.js update

--- food.js
+++ food.js
@@ -1,12 +1,13 @@
 import { onSnake, expandSnake } from "./snake.js";
+import { randomGridPosition } from "./grid.js";
 
-let food = { x: 10, y: 10 };
+let food = getRandomFoodPosition();
 const EXPANSION_RATE = 1; // Esta constante va a definir cuanto crece la serpiente cada vez que come
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE);
-        food = { x: -10, y: -10 };
+        food = getRandomFoodPosition();
     }
 };
 
@@ -16,4 +17,12 @@ export function draw(gameBoard) {
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add("food");
     gameBoard.appendChild(foodElement);
+};
+
+function getRandomFoodPosition() { // Funcion encargada de mover la comidad a un lugar al azar dentro del grid cada vez que la serpiente la come
+    let newFoodPosition;
+    while (newFoodPosition == null || onSnake(newFoodPosition)) { // Mientras que la nueva posicion sea null o este en alguna posicion donde se encuentre la serpiente, va a entrar al while
+        newFoodPosition = randomGridPosition(); // randomGridPosition() va a buscar una nueva posicion continuamente hasta que encuentre un lugar en el grid donde aparecer
+    };
+    return newFoodPosition;
 };
